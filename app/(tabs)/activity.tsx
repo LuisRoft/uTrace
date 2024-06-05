db9903453cffc2ce15431efd7e0c40d3724e5b23
MiguelZea12 <aroryt02@gmail.com>
2024-06-05 04:43:45 -0500

    Image svg Components

--- app/(tabs)/activity.tsx
+++ app/(tabs)/activity.tsx
@@ -1,10 +1,27 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { styles } from '@/styles/activityStyles';
+import CaraFeliz from '@/components/icons/caraFeliz'; 
+import CaraTriste from '@/components/icons/caraTriste';
+import CaraEnojado from '@/components/icons/caraEnojado';
+import CaraMiedo from '@/components/icons/caraMiedo';
+import CaraAsco from '@/components/icons/caraAsco';
+import CaraNose from '@/components/icons/caraNose';
 
-export default function Activity() {
+export default function Activity () {
   return (
-    <View>
-      <ThemedText type='title'>Activity View</ThemedText>
-    </View>
+    <ThemedView style={styles.container}>
+      <ScrollView>
+      <View style={styles.inner}>
+        <ThemedText type="subtitle" darkColor="#000" style={styles.title}>
+          Actividad
+        </ThemedText>
+      </View>
+      </ScrollView>
+    </ThemedView>
   );
 }
+
+
