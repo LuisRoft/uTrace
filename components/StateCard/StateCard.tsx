1db40f0011aeb5ef52710b5776f6ab287fad6afe
Jose Teran <josealwachu9@gmail.com>
2024-07-16 20:05:41 -0500

    StateCard Update

--- components/StateCard/StateCard.tsx
+++ components/StateCard/StateCard.tsx
@@ -5,42 +5,142 @@ import { StateCardProps } from '@/types/StateCardTypes';
 import { styles } from '@/styles/StateCard/stateCardStyles';
 
 const StateCards: React.FC<StateCardProps> = ({
-  colorFlag, textColor, date, hour, flags, activities, EmotionComponent, emotionProps, backgroundColor, customWidth, customHeight 
+  colorFlag, textColor, date, hour, flags, activities, EmotionComponent, emotionProps, backgroundColor, customWidth, customHeight
 }) => {
-  const totalCost = activities.reduce((sum, activity) => sum + activity.cost, 0);
+
+  let mood;
+
+  switch (backgroundColor) {
+    case '#F9F4E2':
+      colorFlag = '#FECD5D';
+      textColor = '#000000';
+      mood = "FELIZ";
+      break;
+
+    case '#8DAEEB':
+      colorFlag = '#4B72FE';
+      textColor = '#FFFFFF';
+      mood = "TRISTE";
+      break;
+
+    case '#f9ac8c':
+      colorFlag = '#FE814B';
+      textColor = '#FFFFFF';
+      mood = "ENOJADO";
+      break;
+
+    case '#E7C1FE':
+      colorFlag = '#A44BFE';
+      textColor = '#000000';
+      mood = "ASUSTADO";
+      break;
+
+    case '#85E0A3':
+      colorFlag = '#48F301';
+      textColor = '#000000';
+      mood = "ASQUEADO";
+      break;
+
+    case '#EAFAFF':
+      colorFlag = '#C4D6FE';
+      textColor = '#000000';
+      mood = "NEUTRAL";
+      break;
+
+    default:
+      mood = "DESCONOCIDO"; // Opcional: para manejar colores no especificados
+      break;
+  }
+
+  const formatDateTime = (dateString: string, timeString: string) => {
+    const date = new Date(dateString + 'T' + timeString);
+    const months = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+
+    let hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    const formattedDate = `${day} DE ${month}`;
+    const formattedTime = `${hours}:${minutes} ${ampm}`;
+
+    return { date: formattedDate, time: formattedTime };
+  };
+
+  const { date: formattedDate, time: formattedTime } = formatDateTime(date, hour);
+
+  switch (backgroundColor) {
+    case '#F9F4E2':
+      colorFlag = '#FECD5D';
+      textColor = '#000000';
+      break;
+
+    case '#8DAEEB':
+      colorFlag = '#4B72FE';
+      textColor = '#FFFFFF';
+      break;
+
+    case '#f9ac8c':
+      colorFlag = '#FE814B';
+      textColor = '#FFFFFF';
+      break;
+
+    case '#E7C1FE':
+      colorFlag = '#A44BFE';
+      textColor = '#FFFFFF';
+      break;
+
+    case '#85E0A3':
+      colorFlag = '#48F301';
+      textColor = '#000000';
+      break;
+
+    case '#EAFAFF':
+      colorFlag = '#C4D6FE';
+      textColor = '#000000';
+      break;
+  }
 
   return (
     <View style={[styles.container, { backgroundColor, width: customWidth, height: customHeight }]}>
-      <View style={styles.leftCont}>
+      <View style={styles.left}>
         <EmotionComponent {...emotionProps} />
       </View>
-      <View style={styles.centerCont}>
-        <View style={styles.date}>
-          <Text style={{ fontSize: 12, color: textColor }}>{date}</Text>
-          <Text style={{ fontSize: 12, color: textColor }}>{hour}</Text>
+
+      <View style={styles.right}>
+        <View style={styles.title}>
+          <Text style={
+            {
+              color: textColor,
+              fontSize: 14,
+            }
+          }>TE SENTISTE {mood}</Text>
         </View>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={styles.grid}>
-            {flags.map((flag, index) => (
-              <Flags key={index} flags={flag} flagColor={colorFlag} textColor={textColor} />
-            ))}
-          </View>
-        </ScrollView>
-      </View>
-      <View style={styles.rightCont}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={styles.activities}>
-            {activities.map((activity, index) => (
-              <View key={index}>
-                <Text style={{ fontSize: 10 }}>{activity.activity}</Text>
-              </View>
-            ))}
-          </View>
-        </ScrollView>
-        <View style={[styles.costs, { backgroundColor: colorFlag }]}>
-          <Text style={{ fontSize: 7, fontWeight: 'bold', color: textColor }}>GASTO TOTAL:</Text>
-          <Text style={{ fontSize: 10, fontWeight: 'bold', color: textColor }}>${totalCost}</Text>
+
+        <View style={styles.emotions}>
+          {flags.slice(0, 3).map((flag, index) => (
+            <Flags key={index} flags={flag} flagColor={colorFlag} textColor={textColor} />
+          ))}
         </View>
+
+        <View style={styles.activities}>
+          {activities.slice(0, 3).map((flag, index) => (
+            <Flags key={index} flags={flag.activity} flagColor={textColor} textColor={colorFlag} />
+          ))}
+        </View>
+
+        <View style={styles.time}>
+          <Text style={
+            {
+              color: textColor,
+              fontSize: 14,
+            }
+          }>{formattedTime}</Text>
+        </View>
+
       </View>
     </View>
   );
